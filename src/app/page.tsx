f10f8ce70d14842edc77dc964c6fa8f7a0af44bf
Marciano Schildmeijer <marciano@kabisa.nl>
2025-03-16 10:53:57 +0100

    feat: welcome to haxball!

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-5xl">Welcome to Kabisa Hax-ball!</h1>
+		</div>
+	);
+}
